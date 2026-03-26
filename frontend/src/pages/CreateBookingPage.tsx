f98c120755961c578a3eca0edea94e4458de20ac
Senja Erlandsson <senjaerlandsson@gmail.com>
2026-03-26 16:17:37 +0100

    style(bookings): add basic styling to CreateBookingPage and BookingForm

--- frontend/src/pages/CreateBookingPage.tsx
+++ frontend/src/pages/CreateBookingPage.tsx
@@ -1,13 +1,20 @@
 import { Navbar } from "../components/Navbar";
 import { BookingForm } from "../features/create-booking/components/BookingForm";
+import bgImage from "../assets/landet2.jpg";
 
 export function CreateBookingsPage() {
   return (
-    <>
-      <Navbar />
-      <div className="p-4">
-        <BookingForm />
+    <div
+      className="min-h-screen bg-cover bg-center pt-4"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+
+        <div className="flex flex-1 justify-center items-center">
+          <BookingForm />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
